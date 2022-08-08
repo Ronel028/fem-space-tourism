106a1bd0ba6d989925d75ba303248765da330683
unknown <shujihanma028@gmail.com>
2022-08-08 05:48:33 -0700

    design destination page

--- src/assets/components/destination.jsx
+++ src/assets/components/destination.jsx
@@ -1,26 +1,117 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Planet from '../images/destination/image-mars.webp'
 
 function Destination(){
+
+    const [active, setActive] = useState({
+        moon: true,
+        mars: false,
+        europa: false,
+        titan: false
+    });
+
+    function handleClick(event){
+        const { name } = event.target;
+        if(name === 'moon'){
+            setActive(function(currentState){
+                return {
+                    ...currentState,
+                    moon: true,
+                    mars: false,
+                    europa: false,
+                    titan: false
+                }
+            })
+        }else if(name === 'mars'){
+            setActive(function(currentState){
+                return {
+                    ...currentState,
+                    moon: false,
+                    mars: true,
+                    europa: false,
+                    titan: false
+                }
+            })
+        }else if(name === 'europa'){
+            setActive(function(currentState){
+                return {
+                    ...currentState,
+                    moon: false,
+                    mars: false,
+                    europa: true,
+                    titan: false
+                }
+            })
+        }else if(name === 'titan'){
+            setActive(function(currentState){
+                return {
+                    ...currentState,
+                    moon: false,
+                    mars: false,
+                    europa: false,
+                    titan: true
+                }
+            })
+        }
+    }
+
+
+
+
+
+
+
     return (
-        <section className="destination--container">
+        <section className="destination--container py-1">
             <div className="destination container">
                 <div className="title">
                     <h2 className="heading-5">
                         <span>01</span>PICK YOUR DESTINATION
                     </h2>
                 </div>
-                <div className="destination-planet">
+                <div className="destination-planet container-md">
                     <div className="planet">
                         <img src={Planet} alt="Mars" />
                     </div>
                     <div className="planet-name">
                         <ul>
-                            <li><a href="#">MOON</a></li>
-                            <li><a href="#">MARS</a></li>
-                            <li><a href="#">EUROPA</a></li>
-                            <li><a href="#">TITAN</a></li>
+                            <li>
+                                <a
+                                    className={active.moon ? 'active' : ''}
+                                    name="moon"
+                                    onClick={handleClick}
+                                >
+                                    MOON
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    className={active.mars ? 'active' : ''}
+                                    name="mars"
+                                    onClick={handleClick}
+                                >
+                                    MARS
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    className={active.europa ? 'active' : ''}
+                                    name="europa"
+                                    onClick={handleClick}
+                                >
+                                    EUROPA
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    className={active.titan ? 'active' : ''}
+                                    name="titan"
+                                    onClick={handleClick}
+                                >
+                                    TITAN
+                                </a>
+                            </li>
                         </ul>
                         {/* PLANET CHOOSE */}
                         <div className="planet-choose">
